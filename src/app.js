3ad61b65406df43d89cd5d1a2701b99a7c65237e
MaRau12 <marthaelisabeth94@gmail.com>
2023-02-11 12:02:12 +0000

    working! with button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const card = document.querySelector(".card");
+const number = document.querySelector(".number");
+let newCard = document.getElementById("btn");
+window.onload = () => {
+  //updateCard();
+  newCard.addEventListener("click", updateCard);
+  //document.querySelector(".card").className = "card " + generateRandomSuit();
+  //document.querySelector(".number").innerHTML = generateRandomValue();
 };
+
+let generateRandomSuit = () => {
+  let suits = ["hearts", "diamonds", "clubs", "spades"];
+  let indexSuit = Math.floor(Math.random() * suits.length);
+  return suits[indexSuit];
+};
+console.log(generateRandomSuit());
+
+let generateRandomValue = () => {
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexValues = Math.floor(Math.random() * values.length);
+  return values[indexValues];
+};
+console.log(generateRandomValue());
+
+function updateCard() {
+  card.className = "card " + generateRandomSuit();
+  number.innerHTML = generateRandomValue();
+}
